84e534b33baf8d445b8a5e07a778661e91f4a113
Just3090 <justnitroso77@outlook.com>
2026-03-15 20:10:53 -0600

    update version code and name

--- renpyandroid/src/main/kotlin/org/renpy/android/AppInfoActivity.kt
+++ renpyandroid/src/main/kotlin/org/renpy/android/AppInfoActivity.kt
@@ -10,7 +10,7 @@ class AppInfoActivity : GameWindowActivity() {
 
     private lateinit var binding: ActivityAppInfoBinding
 
-    private val COMMIT_HASH = "166a970"
+    private val COMMIT_HASH = "de01ba6"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
