9fe8b5128224eb360bd5653b77d346a0121fe0d4
Just3090 <justnitroso77@outlook.com>
2026-09-20 18:28:26 -0400

    edit package and version

--- renpyandroid/src/main/kotlin/org/renpy/android/DiscordRpcManager.kt
+++ renpyandroid/src/main/kotlin/org/renpy/android/DiscordRpcManager.kt
@@ -17,7 +17,7 @@ object DiscordRpcManager {
         "https://raw.githubusercontent.com/New-Traduction-Club/MonikaAfterStory-Android-port/c064f77/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png"
     private const val DEFAULT_RPC_APPLICATION_ID = "962990036020756480"
     private const val PLAY_STORE_URL =
-        "https://play.google.com/store/apps/details?id=com.z.mas.portby.just6889"
+        "https://play.google.com/store/apps/details?id=com.z.tdclub.mine.se"
     private const val GITHUB_REPO_URL =
         "https://github.com/New-Traduction-Club/MonikaAfterStory-Android-port"
     const val PREF_DISCORD_RPC_ENABLED = "discord_rpc_enabled"
